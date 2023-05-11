46f3708f794d32e06499f38c0b5872ec2a9c0eaa
Jan-Timmothy Lukban <111472753+Timmothy13@users.noreply.github.com>
2023-05-11 09:54:18 +0800

    Add files via upload

---
+++ assignment3/Pet.java
@@ -0,0 +1,7 @@
+public class Pet  implements Burian{
+
+    @Override
+    public void meow(){
+        System.out.println("My Burian is lazy");
+    }
+}
---
+++ assignment3/Siberian.java
@@ -0,0 +1,5 @@
+interface Siberian{
+
+     String catColor= "White";
+     void meow();
+}
